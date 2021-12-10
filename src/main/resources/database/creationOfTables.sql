7fc9b8d3d6c05ebd0e0e68bdd87bbabbb9fb4ce6
tumolabsproject4 <tumo4.labs@gmail.com>
2021-12-10 11:25:11 -0800

    initial page

--- src/main/resources/database/creationOfTables.sql
+++ src/main/resources/database/creationOfTables.sql
@@ -3,29 +3,29 @@ CREATE DATABASE IF NOT EXISTS banking_ve;
 CREATE TABLE IF NOT EXISTS user
 (
     user_id       BIGINT PRIMARY KEY auto_increment,
-    username      VARCHAR(255) NOT NULL UNIQUE,
-    user_password VARCHAR(255) NOT NULL UNIQUE
+    username      VARCHAR(255) ,
+    user_password VARCHAR(255)
 );
 
 CREATE TABLE IF NOT EXISTS bank
 (
     bank_id   BIGINT PRIMARY KEY auto_increment,
-    bank_name VARCHAR(255) NOT NULL UNIQUE,
-    address   VARCHAR(255) NOT NULL UNIQUE,
+    bank_name VARCHAR(255) ,
+    address   VARCHAR(255) ,
     image     BLOB
 );
 
 CREATE TABLE IF NOT EXISTS employee
 (
     employee_id      BIGINT PRIMARY KEY auto_increment,
-    first_name       VARCHAR(255) NOT NULL UNIQUE,
-    last_name        VARCHAR(255) NOT NULL UNIQUE,
-    age              INT          NOT NULL UNIQUE,
-    salary           FLOAT        NOT NULL UNIQUE,
-    address          VARCHAR(255) NOT NULL UNIQUE,
-    department       VARCHAR(255) NOT NULL UNIQUE,
-    employees_status BIT          NOT NULL UNIQUE,
-    bank_id          BIGINT       NOT NULL UNIQUE,
+    first_name       VARCHAR(255) ,
+    last_name        VARCHAR(255) ,
+    age              INT          ,
+    salary           FLOAT        ,
+    address          VARCHAR(255) ,
+    department       VARCHAR(255) ,
+    employees_status BIT          ,
+    bank_id          BIGINT       ,
     image            BLOB,
     FOREIGN KEY (bank_id) REFERENCES bank (bank_id)
 );
